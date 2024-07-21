dadcd6e53d99ca60d239de209432978fae021c85
ca1o5r <ae20wre36@gmail.com>
2024-07-21 10:03:53 +0800

    初始版本

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Customize-Android
-Android全局使用定制字体 - Magisk模块
+# Android全局使用定制字体 - Magisk模块
+Android全局使用定制字体，可根据语言文字使用不同字体（/etc/fonts.conf中配置）
+以及配合[killgmsfont](https://github.com/MrCarb0n/killgmsfont)模块禁用GMS的字体服务（用于Google应用使用）
+
+![](screenshots/01.png)
+![](screenshots/02.png)
+![](screenshots/03.png)
+![](screenshots/04.png)
+![](screenshots/05.png)
+
---
+++ service.sh
@@ -0,0 +1,57 @@
+#!/system/bin/sh
+# force twitter use system fonts
+chown shell:shell guo-rw /data/data/com.twitter.android/files
+
+# ╭────────────────────────────────────────────╮
+# │     Kill GMS Font Module. | @MrCarb0n      │
+# ├────────────────────────────────────────────┤
+# │   Give proper credit before distribution   │
+# │     or modification. All files licensed    │
+# │   under GNU General Public License v3.0    │
+# ├────────────────────────────────────────────┤
+# │  https://github.com/MrCarb0n/killgmsfont   │
+# ╰────────────────────────────────────────────╯
+
+# set -x
+
+while true; do
+    (
+        # Wait until proper boot up
+        until [ "$(resetprop sys.boot_completed)" = "1" -a -d "/data" ]; do
+            sleep 1
+        done
+
+        # Disable GMS' font service
+        STATE_GMSF() {
+            PM="$(command -v pm)"
+            GMSF="com.google.android.gms/com.google.android.gms.fonts"
+
+            UPS=$(ls -d /data/user/*)  # Dynamically get user profiles
+
+            for UP in $UPS; do
+                "$PM" "$@" --user "${UP##*/}" "$GMSF.update.UpdateSchedulerService"
+                "$PM" "$@" --user "${UP##*/}" "$GMSF.provider.FontsProvider"
+            done
+        }
+
+        # Delete GMS' generated fonts
+        DEL_GMSF() {
+            local GMSFD=com.google.android.gms/files/fonts
+
+            [ -d /data/fonts ] && rm -r /data/fonts
+
+            find /data \
+                -type d \
+                -path "*$GMSFD*" \
+                -exec test -d {} \; -exec rm -r {} \;
+        }
+
+        # Run primary tasks
+        STATE_GMSF disable >/dev/null 2>&1
+        DEL_GMSF
+    )
+
+    # Sleep for 3 hours before running again
+    sleep 10800 # 3 hours in seconds
+done
+
