64f89cb86332b7cba0e904a989e137069b90b8b7
ca1o5r <ae20wre36@gmail.com>
2024-07-27 08:30:51 +0800

    增加功能： Android 6启动动画； 定时删除垃圾文件； 禁用OEM加密（允许播放DRM保护内容）；

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# Android全局使用定制字体 - Magisk模块
-Android全局使用定制字体，可根据语言文字使用不同字体（system/etc/fonts.xml中配置）
-以及配合[killgmsfont](https://github.com/MrCarb0n/killgmsfont)模块禁用GMS的字体服务（用于Google应用使用）
+# 自定义Android - Magisk模块 
+功能：
+1. Android全局使用定制字体，可根据语言文字使用不同字体（system/etc/fonts.xml中配置）
+以及参考[killgmsfont](https://github.com/MrCarb0n/killgmsfont)模块禁用GMS的字体服务（用于Google应用）
+2. Android 6启动动画；
+3. 定时删除垃圾文件；
+4. 禁用OEM加密（允许播放DRM保护内容）；
 
 ![](screenshots/01.png)
 ![](screenshots/02.png)
--- service.sh
+++ service.sh
@@ -1,59 +1,75 @@
-#!/system/bin/sh
-# force twitter use system fonts
-chown shell:shell /data/data/com.twitter.android/files
-chmod guo-rwx /data/data/com.twitter.android/files
-chmod gu+r /data/data/com.twitter.android/files
 
-# ╭────────────────────────────────────────────╮
-# │     Kill GMS Font Module. | @MrCarb0n      │
-# ├────────────────────────────────────────────┤
-# │   Give proper credit before distribution   │
-# │     or modification. All files licensed    │
-# │   under GNU General Public License v3.0    │
-# ├────────────────────────────────────────────┤
-# │  https://github.com/MrCarb0n/killgmsfont   │
-# ╰────────────────────────────────────────────╯
+while true
+do
 
-# set -x
-
-while true; do
-    (
-        # Wait until proper boot up
-        until [ "$(resetprop sys.boot_completed)" = "1" -a -d "/data" ]; do
-            sleep 1
+    until [ "$(resetprop sys.boot_completed)" = "1" -a -d "/data" ]; do
+        sleep 5
+    done
+    
+    rm -rf /sdcard/Pictures/.gs*
+    rm -rf /sdcard/Pictures/.thumb*
+    rm -rf /sdcard/Pictures/WeChat
+    rm -rf /sdcard/Pictures/WeiXin
+    rm -rf /sdcard/Pictures/CloudHub
+    rm -rf /sdcard/Pictures/Douban
+    rm -rf /sdcard/Pictures/weibo*
+    rm -rf /sdcard/Music/.thumb*
+    rm -rf /sdcard/DCIM/.tmfs
+    rm -rf /sdcard/DCIM/.android
+    rm -rf /sdcard/DCIM/.yz
+    rm -rf /sdcard/Movies
+    rm -rf /sdcard/Android/media
+    rm -rf /sdcard/Android/.vy
+    rm -rf /sdcard/Android/obb
+    rm -rf /sdcard/Android/obj
+    rm -rf /sdcard/Android/.data
+    rm -rf /sdcard/Android/data/.nomedia
+    rm -rf /sdcard/Documents/.thumb*
+    rm -rf /sdcard/Download/.thumb*
+    rm -rf /sdcard/Download/.tistore*
+    rm -rf /sdcard/Download/Nearby*
+    rm -rf /sdcard/Download/netease*
+    rm -rf /sdcard/YZJLog
+    rm -rf /sdcard/.yzj_id
+    rm -rf /sdcard/.vivo
+    rm -rf /sdcard/.gs_fs0
+    rm -rf /sdcard/.DataStorage
+    rm -rf /sdcard/.UTSystemConfig
+    rm -rf /sdcard/msc
+    rm -rf /sdcard/tencent
+    
+    su -c "chown shell:shell /data/data/com.twitter.android/files"
+    su -c "chmod guo-rwx /data/data/com.twitter.android/files"
+    su -c "chown shell:shell /data/data/cmb.pb/files/fontResource"
+    su -c "chmod guo-rwx /data/data/cmb.pb/files/fontResource"
+    su -c "chown shell:shell /data/data/com.xiaomi.smarthome/files/dynamic_fonts"
+    su -c "chmod guo-rwx /data/data/com.xiaomi.smarthome/files/dynamic_fonts"
+    
+    STATE_GMSF() {
+        PM="$(command -v pm)"
+        GMSF="com.google.android.gms/com.google.android.gms.fonts"
+        
+        UPS=$(ls -d /data/user/*)
+        
+        for UP in $UPS; do
+            "$PM" "$@" --user "${UP##*/}" "$GMSF.update.UpdateSchedulerService"
+            "$PM" "$@" --user "${UP##*/}" "$GMSF.provider.FontsProvider"
         done
+    }
 
-        # Disable GMS' font service
-        STATE_GMSF() {
-            PM="$(command -v pm)"
-            GMSF="com.google.android.gms/com.google.android.gms.fonts"
+    DEL_GMSF() {
+        local GMSFD=com.google.android.gms/files/fonts
 
-            UPS=$(ls -d /data/user/*)  # Dynamically get user profiles
+        [ -d /data/fonts ] && rm -r /data/fonts
+            
+        find /data \
+            -type d \
+            -path "*$GMSFD*" \
+            -exec test -d {} \; -exec rm -r {} \;
+    }
 
-            for UP in $UPS; do
-                "$PM" "$@" --user "${UP##*/}" "$GMSF.update.UpdateSchedulerService"
-                "$PM" "$@" --user "${UP##*/}" "$GMSF.provider.FontsProvider"
-            done
-        }
+    STATE_GMSF disable >/dev/null 2>&1
+    DEL_GMSF
 
-        # Delete GMS' generated fonts
-        DEL_GMSF() {
-            local GMSFD=com.google.android.gms/files/fonts
-
-            [ -d /data/fonts ] && rm -r /data/fonts
-
-            find /data \
-                -type d \
-                -path "*$GMSFD*" \
-                -exec test -d {} \; -exec rm -r {} \;
-        }
-
-        # Run primary tasks
-        STATE_GMSF disable >/dev/null 2>&1
-        DEL_GMSF
-    )
-
-    # Sleep for 3 hours before running again
-    sleep 10800 # 3 hours in seconds
+    sleep 60
 done
-
